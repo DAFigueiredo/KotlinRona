8acf6e36f7591d6d25f80ccdce006875db79b3e2
Diogo Figueiredo <diogo.alvesfigueiredo@gmail.com>
2020-08-28 18:59:11 +0100

    basic version
    game is running
    needs restart

--- app/src/main/java/com/example/kotlinrona/MainActivity.kt
+++ app/src/main/java/com/example/kotlinrona/MainActivity.kt
@@ -2,10 +2,87 @@ package com.example.kotlinrona
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.CountDownTimer
+import android.os.Handler
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+import kotlin.random.Random
 
 class MainActivity : AppCompatActivity() {
+
+    var ronaArray = ArrayList<ImageView>()
+
+    var score: Int = 0
+
+    var handler: Handler = Handler()
+    var runnable: Runnable = Runnable { }
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        score = 0
+
+        ronaArray = arrayListOf(
+            imageView1,
+            imageView2,
+            imageView3,
+            imageView4,
+            imageView5,
+            imageView6,
+            imageView7,
+            imageView8,
+            imageView9
+        )
+
+        hide()
+
+        object : CountDownTimer(20000, 1000) {
+            override fun onTick(p0: Long) {
+                textTime.text = "Time Left: " + p0 / 1000
+            }
+
+            override fun onFinish() {
+                textTime.text = "Time's up!"
+
+                handler.removeCallbacks(runnable)
+
+                for (image in ronaArray) {
+                    image.visibility = View.INVISIBLE
+                }
+            }
+
+        }.start()
+
     }
+
+    fun hide() {
+
+        runnable = object : Runnable {
+            override fun run() {
+
+                for (image in ronaArray) {
+                    image.visibility = View.INVISIBLE
+                }
+
+                val random = java.util.Random()
+                val index = random.nextInt(8 - 0)
+                ronaArray[index].visibility = View.VISIBLE
+
+                handler.postDelayed(runnable, 500)
+            }
+
+        }
+
+        handler.post(runnable)
+    }
+
+    fun score(view: View) {
+            score++
+            textScore.text = "Score: " + score
+        }
+
 }
